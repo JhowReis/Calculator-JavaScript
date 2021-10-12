2db7793fe67a88f611e8fc7cb97a407ec3cbbce5
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-10-12 12:00:30 -0300

    constructor function calculator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,86 @@
-function createCalculator() {
-    return {
-        display: document.querySelector('.display'),
-        // btnClear: document.querySelector('.btn-clear'),
+function Calculator() {
+    this.display = document.querySelector('.display');
 
+    this.start = () => {
+        this.clickBtn();
+        this.pressEnter();
+    }
 
+    this.pressEnter = () => {
+        this.display.addEventListener('keypress', e => {
+            if (e.keyCode !== 13) return;
+            this.result();
+        });
 
-        start() {
-            this.clickBtn();
-            this.pressEnter();
-        },
+    }
 
-        pressEnter(){
-            this.display.addEventListener('keyup', e => {
-                if(e.keyCode === 13){
-                    this.alert('meu ovo')
-                }
-            });
-        },
+    this.clickBtn = () => {
+        document.addEventListener('click', event => {
+            const element = event.target;
+            if (element.classList.contains('btn-number')) this.btnToDisplay(element);
+            if (element.classList.contains('btn-clear')) this.clear();
+            if (element.classList.contains('btn-del')) this.del();
+            if (element.classList.contains('btn-equal')) this.result();
+        });
+    };
 
+    this.btnToDisplay = element => {
+        this.display.value += element.innerText;
+        this.display.focus();
+    }
+    this.clear = () => this.display.value = '';
 
-        clickBtn() {
-            document.addEventListener('click', e => {
-                const element = e.target;
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
 
-                if (element.classList.contains('btn-number')) {
-                    this.btnToDisplay(element.innerText);
-                }
+    this.result = () => {
+        try {
+            const operation = eval(this.display.value);
 
-                if (element.classList.contains('btn-clear')) {
-                    this.display.value = '';
-                }
+            if (!operation) {
+                alert('Invalid operation')
+            }
 
-                if (element.classList.contains('btn-del')) {
-                    this.display.value = this.display.value.slice(0, -1);
-                }
+            this.display.value = operation;
 
-                if (element.classList.contains('btn-equal')) {
-                    
-                }
-            });
-        },
-
-        btnToDisplay(value) {
-            this.display.value += value;
-
-        },
-
+        } catch (e) {
+            alert('Invalid operation');
+            return;
+        }
     };
 }
 
-const calculator = createCalculator();
+// start() {
+//     this.clickBtn();
+
+// },
+
+// clickBtn() {
+//     document.addEventListener('click', e => {
+//         const element = e.target;
+
+//         if (element.classList.contains('btn-number')) {
+//             this.btnToDisplay(element.innerText);
+//         }
+
+//         if (element.classList.contains('btn-clear')) {
+//             this.display.value = '';
+//         }
+
+//         if (element.classList.contains('btn-del')) {
+//             this.display.value = this.display.value.slice(0, -1);
+//         }
+
+//         if (element.classList.contains('btn-equal')) {
+
+//         }
+//     });
+// },
+
+// btnToDisplay(value) {
+//     this.display.value += value;
+
+
+
+
+const calculator = new Calculator();
 calculator.start();
